0972905bd444f67733e217e4c595c3278c8a074c
sakibit17043 <[it17043@mbstu.ac.bd]>
2022-06-28 03:57:43 +0600

    front part done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
     return (
         <div>
-            <h1>This is home</h1>
+            <div className='main'>
+                <div>
+
+            <h1>Time To Be Smart</h1>
+            <p>A smartwatch is a portable device that's designed to be worn on a wrist. Like smartphones, they use touchscreens, offer apps, and often record your heart rate and other vital signs.</p>
+                </div>
+                <div>
+
+            <img src={process.env.PUBLIC_URL+"images/Mibro-Lite-Smart-Watch.jpg" }/>
+                </div>
+            
+
+            </div>
         </div>
     );
 };
