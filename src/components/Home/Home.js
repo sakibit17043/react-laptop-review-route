8aa37a9b9cee4beca71601f7df2c5675c5a5f55b
sakibit17043 <[it17043@mbstu.ac.bd]>
2022-06-29 01:07:40 +0600

    img correction

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,8 +23,7 @@ const Home = () => {
             <p>A smartwatch is a portable device that's designed to be worn on a wrist. Like smartphones, they use touchscreens, offer apps, and often record your heart rate and other vital signs.</p>
                 </div>
                 <div>
-
-            <img src={process.env.PUBLIC_URL+"images/Mibro-Lite-Smart-Watch.jpg" }/>
+                <img src="images/Mibro-Lite-Smart-Watch.jpg" alt="" />
                 </div>
             
 
