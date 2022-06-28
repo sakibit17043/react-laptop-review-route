8b36cbf90b069da27b1a95af164e36c0e0ae8eb3
sakibit17043 <[it17043@mbstu.ac.bd]>
2022-06-29 02:43:10 +0600

    blogs part added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>This is blogs.</h1>
+            <h1>1.What is context api?</h1>
+            <h2>Ans:-
+                Context api is react structure that helps to share data without props drilling.We know that in react application with a lot of nested cmponents to pass a data to child component from nth level it is very difficult.But by context api we can share information to all the nested componets from declare only in parent componets.And we can use it from any components without props drilling.
+            </h2>
+            <h1>What is semantic tag?</h1>
+            <h2>Ans:-The semantic tag is the purpose of understanding the meaning of tag.Semantic tag clearly defines its content.It is easier to read.It has a great accessibility.We use semantic tag for better experience for developer.</h2>
         </div>
     );
 };
